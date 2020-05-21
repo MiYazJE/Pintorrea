65dc68d97cd5df02c9031e5e43c7b99c6101c087
MiYazJE <ruben.saizserrano@gmail.com>
2020-05-21 23:49:22 +0200

    Upload user avatar in base64

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,14 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
 import "../css/profile.css";
-import { Avatar } from "antd";
-import { UserOutlined } from "@ant-design/icons";
+import { Upload, message } from "antd";
+import { LoadingOutlined, PlusOutlined } from "@ant-design/icons";
+import Http from '../Helpers/Http';
+import { connect } from 'react-redux';
+import { logUser } from '../Redux/Actions/UserActions';
 
-export default function Profile({ user }) {
+function getBase64(img, callback) {
+    const reader = new FileReader();
+    reader.addEventListener('load', () => callback(reader.result));
+    reader.readAsDataURL(img);
+}
+
+function beforeUpload(file) {
+    const isJpgOrPng = file.type === 'image/jpeg' || file.type === 'image/png';
+    if (!isJpgOrPng) {
+        message.error('Solo puedes subir ficheros jpeg y png!');
+    }
+    const isLt2M = file.size / 1024 / 1024 < 2;
+    if (!isLt2M) {
+        message.error('La imágen no debe superar 2mb!');
+    }
+    return isJpgOrPng && isLt2M;
+}
+
+const Profile = ({ user, logUser }) => {
+    const [loading, setLoading] = useState(false);
+    const [imageUrl, setImageUrl] = useState(user.picture);
     console.log(user)
+
+    const handleChange = info => {
+        if (info.file.status === 'uploading') {
+            setLoading(true);
+            return;
+        }
+        if (info.file.status === 'done') {
+            getBase64(info.file.originFileObj, async (picture) => {
+                const newUser = await Http.post({ picture, id: user.id }, '/user/uploadPicture');
+                message.success('La imágen ha sido actualizada');
+                setLoading(false);
+                setImageUrl(picture);
+            });
+        }
+    };
+
+    const uploadButton = (
+        <div>
+          {loading ? <LoadingOutlined /> : <PlusOutlined />}
+          <div className="ant-upload-text">Upload</div>
+        </div>
+    );
+
     return (
         <div className="wrapProfile">
-            <Avatar size={150}  src={user.picture && user.picture} icon={!user.picture && <UserOutlined />} />
+            <Upload
+                name="avatar"
+                listType="picture"
+                className="avatar-uploader"
+                showUploadList={false}
+                action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+                beforeUpload={beforeUpload}
+                onChange={handleChange}
+            >
+                {imageUrl ? <img src={imageUrl} alt="avatar" style={{ width: '50%' }} /> : uploadButton}
+            </Upload>
             <h2>{user.name}</h2>
         </div>
     );
 }
+
+export default connect(null, { logUser })(Profile);
