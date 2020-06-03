b5fe93a6cd4e1da6cb1c5f41d6129fe86eb60eed
MiYazJE <ruben.saizserrano@gmail.com>
2020-06-03 20:38:28 +0200

    Disable redux devtool for production

--- client/src/Redux/Store.js
+++ client/src/Redux/Store.js
@@ -3,11 +3,15 @@ import reducers from './Reducers';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
+console.log(process.env.REACT_APP_ENVIROMENT)
+const devTools = 
+    process.env.REACT_APP_ENVIROMENT === 'PRODUCTION'
+        ? applyMiddleware(thunk)
+        : composeWithDevTools(applyMiddleware(thunk));
+
 const store = createStore(
     reducers, 
-    composeWithDevTools(
-        applyMiddleware(thunk)
-    )
+    devTools
 );
 
 export default store;
